11a3f9a4bcfe696ee0759b51e8b0c752ebe68ac0
x414i <muhammadbaled20@gmail.com>
2025-01-24 11:02:23 +0200

    Changed run App To Login Form

--- School_Management/Program.cs
+++ School_Management/Program.cs
@@ -17,7 +17,7 @@ namespace School_Management
            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
 
         }
     }
